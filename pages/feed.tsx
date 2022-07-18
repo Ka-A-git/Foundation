459528358bbc8f7e9347a1b70682de5ebc430734
Nabi <46531267+nabi1020-dev@users.noreply.github.com>
2022-07-18 10:41:32 +0800

    Update feed.tsx

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'; 
 import { GetStaticPropsResult } from 'next';
 import { useRouter } from 'next/router';
 import { is } from 'ramda';
